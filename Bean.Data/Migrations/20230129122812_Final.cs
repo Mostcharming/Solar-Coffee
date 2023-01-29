df388a1e23b241a394c7e266af4e94984e2207f9
Mayowa F Fredrick <mostcharming920@yahoo.com>
2023-01-29 18:20:01 +0100

    Unit tests

---
+++ Bean.Data/Migrations/20230129122812_Final.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bean.Data.Migrations
+{
+    public partial class Final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerId",
+                table: "SalesOrders",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesOrders_CustomerId",
+                table: "SalesOrders",
+                column: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SalesOrders_Customers_CustomerId",
+                table: "SalesOrders",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SalesOrders_Customers_CustomerId",
+                table: "SalesOrders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SalesOrders_CustomerId",
+                table: "SalesOrders");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "SalesOrders");
+        }
+    }
+}
